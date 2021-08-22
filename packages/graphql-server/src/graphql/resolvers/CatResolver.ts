56df584eacbee5d39610027b465484f9ab96bb2b
olvnikon <olvnikon@gmail.com>
2021-08-22 13:33:25 +0200

    feat: improve cat resolver types

--- packages/graphql-server/src/graphql/resolvers/CatResolver.ts
+++ packages/graphql-server/src/graphql/resolvers/CatResolver.ts
@@ -1,5 +1,6 @@
 import { Resolver, Query, Arg } from 'type-graphql';
-import { Cat } from '../types';
+import { flow } from 'fp-ts/lib/function';
+import { Cat, Owner, Comment } from '../types';
 import db from '../db.json';
 
 type catType = typeof db.cats[number];
@@ -8,19 +9,27 @@ type catType = typeof db.cats[number];
 export class CatResolver {
   @Query(() => [Cat])
   cats() {
-    return db.cats.map(this._attachOwnerAndComments);
+    return db.cats.map(this._attachEntities);
   }
 
   @Query(() => Cat)
   cat(@Arg('id', () => Number) id: number) {
     const cat = db.cats.find((cat) => cat.id === id);
-    return cat ? this._attachOwnerAndComments(cat) : undefined;
+    return cat ? this._attachEntities(cat) : undefined;
   }
 
-  private _attachOwnerAndComments(cat: catType) {
+  private _attachEntities = flow(this._attachOwner, this._attachComments);
+
+  private _attachOwner<T extends catType>(cat: T): T & { owner: Owner } {
     return {
       ...cat,
-      owner: db.owners.find((owner) => owner.id === cat.ownerId),
+      owner: db.owners.find((owner) => owner.id === cat.ownerId)!,
+    };
+  }
+
+  private _attachComments<T extends catType>(cat: T): T & { comments: Comment[] } {
+    return {
+      ...cat,
       comments: db.comments.filter((comment) => comment.cat === cat.id),
     };
   }
